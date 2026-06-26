ec25d688d9b8ed8fde097fda4c3407349c2de2de
giacoren6 <142323106+giacoren6@users.noreply.github.com>
2026-06-26 11:15:44 +0000

    add create db users

--- nhs_trust_db/nhs_trust_security.sql
+++ nhs_trust_db/nhs_trust_security.sql
@@ -10,3 +10,13 @@ DROP USER IF EXISTS 'admin_user'@'localhost';
 DROP USER IF EXISTS 'doctor_user'@'localhost';
 DROP USER IF EXISTS 'reception_user'@'localhost';
 DROP USER IF EXISTS 'patient_user'@'localhost';
+
+-- ==========================================
+-- Create database users
+-- ==========================================
+
+CREATE USER 'admin_user'@'localhost' IDENTIFIED BY 'Admin1!';
+CREATE USER 'doctor_user'@'localhost' IDENTIFIED BY 'Doctor1!';
+CREATE USER 'reception_user'@'localhost' IDENTIFIED BY 'Reception1!';
+CREATE USER 'patient_user'@'localhost' IDENTIFIED BY 'Patient1!';
+
