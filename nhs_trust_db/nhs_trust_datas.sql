333c694abcb3c3266f2b83a72a151ec709364822
giacoren6 <142323106+giacoren6@users.noreply.github.com>
2026-06-26 10:31:19 +0000

    add datas values into clinics

---
+++ nhs_trust_db/nhs_trust_datas.sql
@@ -0,0 +1,6 @@
+-- 1. CLINICS
+INSERT INTO Clinics (ClinicName, ClinicAddress, PhoneNumber, Email) VALUES 
+('Harley Street Clinic', '12 Harley St, London', 2071110001, 'info@harleyst.co.uk'),
+('Westminster Medical', '50 Victoria St, London', 2072220002, 'info@westminster.co.uk'),
+('City Health Hub', '100 Bishopsgate, London', 2073330003, 'info@cityhealth.co.uk');
+ 
